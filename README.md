58305cbae0836e991524a04258d2acc8b0b00200
Dan Kaplun <dbkaplun@gmail.com>
2018-02-06 21:12:49 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-Github Markdown Outline Extension 
+Github Sort by Comment Reactions Extension
 ======
-Displays a clickable outline of all topic headers for markdown documents on Github &mdash; *Install via [Chrome Web Store](https://chrome.google.com/webstore/detail/github-markdown-outline-e/gccinjjdbfdkkkebfbeipopijjfohfgj).*
+Allows you to sort by comment reaction count in issues and PRs on Github &mdash; *TODO: Install via Chrome Web Store.*
 
-![Screenshot](https://raw.githubusercontent.com/dbkaplun/github-markdown-outline-extension/master/screenshot.png)
+TODO: Screenshot
 
 ## Installation
 
-Install via [Chrome Web Store](https://chrome.google.com/webstore/detail/github-markdown-outline-e/gccinjjdbfdkkkebfbeipopijjfohfgj).
+TODO: Install via Chrome Web Store.
 
 ## Usage
 
-When you visit any page with markdown on Github, this extension will automatically display a clickable outline near the top of the markdown document.
+When you visit any issues or PRs page on Github, this extension will provide an option to sort comments by reaction count.
 
-To see all available options, copy and paste the following URL into Chrome: `chrome://extensions/?options=gccinjjdbfdkkkebfbeipopijjfohfgj`
+TODO: To see all available options, copy and paste the following URL into Chrome: `chrome://extensions/?options=gccinjjdbfdkkkebfbeipopijjfohfgj`
 
 ## Development
 
-* `git clone git@github.com:dbkaplun/github-markdown-outline-extension.git`
+* `git clone git@github.com:dbkaplun/github-sort-by-comment-reactions-extension.git`
 * Go to `chrome://extensions` in Chrome
 * Ensure the *Developer mode* checkbox is clicked
 * Click the *Load unpacked extension...* button
-* Select the `github-markdown-outline-extension` directory you just cloned
+* Select the `github-sort-by-comment-reactions-extension` directory you just cloned
 * Make sure the extension was added by refreshing this page
--- index.js
+++ index.js
@@ -1,77 +1,59 @@
-(root => {
-  var document = root.document
+(({document}) => {
+  const qsa = ($el, sel) => [ ...$el.querySelectorAll(sel) ];
 
-  getHeaderLevel.REGEXP = /h(\d)/i
-  function getHeaderLevel ($h) {
-    var level = Number(((($h || {}).tagName || '').match(getHeaderLevel.REGEXP) || [])[1])
-    return isNaN(level) ? undefined : level
+  const commentSorts = [
+    {
+      name: "date posted",
+      fn: ($comment) => new Date($comment.querySelector('relative-time').getAttribute('datetime'))
+    },
+    {
+      name: "reaction count",
+      fn: ($comment) => (
+        qsa($comment, '.reaction-summary-item').reduce((total, $reaction) => {
+          const countForReaction = parseInt(($reaction.innerText.match(/\d+$/) || ['0'])[0].trim());
+          return total - countForReaction;
+        }, 0)
+      )
+    },
+  ];
+  const nextSort = (sort) => (sort+1)%commentSorts.length;
+
+  function sortComments($discussion, sort) {
+    const fn = commentSorts[sort].fn;
+    const $sortedComments = qsa($discussion, '.js-comment-container')
+      .sort(($a, $b) => fn($a) - fn($b));
+
+    $discussion.innerHTML = '';
+    $sortedComments.forEach($comment => { $discussion.appendChild($comment); });
+  }
+  function setButtonText($btn, sort) {
+    $btn.innerText = `Sort by ${commentSorts[nextSort(sort)].name}`;
   }
 
-  var headerSels = []
-  for (var l = 1; l <= 6; l++) headerSels.push('h'+l)
-  var headerSel = headerSels.join(', ')
-  var anchorSel = 'a[id]'
-  var linkSel = 'a[href^="#"]:not(.anchor)'
+  function render() {
+    const $discussions = qsa(document, '.js-discussion');
+    const $headers = qsa(document, '.gh-header-actions');
 
-  root.chrome.storage.sync.get({
-    // default values
-    hideIfOutlineDetected: true,
-    float: false,
-    useInnerHTML: true
-  }, options => {
-    Array.from(document.querySelectorAll('.markdown-body')).forEach($md => {
-      var $headers = Array.from($md.querySelectorAll(headerSel))
+    $headers.forEach($header => {
+      $header.innerHTML += `<button class="btn btn-sm float-right sort-btn" data-hotkey="o" />`;
+      const $sortBtn = $header.querySelector('.sort-btn');
+  
+      let sort = 0;
+      setButtonText($sortBtn, sort);
+      $sortBtn.addEventListener('click', () => {
+        $discussions.forEach($discussion => {
+          sort = nextSort(sort);
+          sortComments($discussion, sort);
+          setButtonText($sortBtn, sort);
+        });
+      });
+    });
+  }
 
-      if (options.hideIfOutlineDetected && $md.querySelectorAll(linkSel).length >= Math.max($headers.length - 10, 0)) return // there's already an outline in the document
-
-      var $container = document.createElement('div')
-      $container.classList.add('__github-markdown-outline-container')
-      if (options.float) $container.classList.add('__github-markdown-outline-container--float')
-
-      var $outline = document.createElement('ul')
-      $outline.classList.add('__github-markdown-outline')
-      $container.appendChild($outline)
-
-      // generate outline from headers
-      $headers.forEach($h => {
-        var level = getHeaderLevel($h)
-        if (!level) return
-        var $ul = $outline, $li, $child
-        for (var l = 1; l < level; l++) {
-          $li = $ul.lastChild || $ul.appendChild(document.createElement('li'))
-          $child = $li.lastChild || {}
-          $ul = $child.tagName === 'UL'
-            ? $child
-            : $li.appendChild(document.createElement('ul'))
-        }
-        var $topic = $ul
-          .appendChild(document.createElement('li'))
-          .appendChild(document.createElement('a'))
-        if (options.useInnerHTML) {
-          $topic.innerHTML = $h.innerHTML
-          Array.from($topic.querySelectorAll(anchorSel)).forEach($child => {
-            $child.parentNode.removeChild($child)
-          })
-        } else {
-          $topic.innerText = $h.innerText
-        }
-        $topic.href = `#${$h.querySelector(anchorSel).id}`
-      })
-
-      // find all sublists with one item and replace with contents
-      Array.from($container.querySelectorAll('ul')).forEach($ul => {
-        var $parent = $ul.parentNode
-        var $li = $ul.firstChild
-        var $child = $li.firstChild
-        if ($li !== $ul.lastChild || $child.tagName !== 'UL') return
-        while ($child) {
-          $parent.insertBefore($child, $ul.nextSibling) // inserts to end of list if $ul.nextSibling is null
-          $child = $child.nextSibling
-        }
-        $parent.removeChild($ul)
-      })
-
-      $md.insertBefore($container, $md.firstChild)
-    })
-  })
-})(this)
+  document.addEventListener('pjax:complete', render);
+  document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "complete") {
+      render();
+    }
+  });
+})(this);
